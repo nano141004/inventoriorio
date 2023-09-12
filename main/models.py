ced1420cd80574530cdef5b9689b40dde3e43c55
nano <marianosenduk@gmail.com>
2023-09-12 11:02:11 +0700

    modified models, view, and template

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    date_added = models.DateField(auto_now_add=True)
+    amount = models.IntegerField(default=0)
     price = models.IntegerField()
     description = models.TextField()
+    
--- main/views.py
+++ main/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'name': 'Mariano Gerardus Senduk',
-        'class': 'PBP F'
+        'class': 'PBP F',
+        'produk1' : 'inventoriorio'
     }
 
     return render(request, "main.html", context)
