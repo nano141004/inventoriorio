50010d1a49357219592ee5a28265cfa33ef40742
nano <marianosenduk@gmail.com>
2023-09-26 15:06:28 +0700

    finish all task including bonus

--- main/models.py
+++ main/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
 class Item(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    amount = models.IntegerField(default=0)
+    amount = models.IntegerField(default=1, validators=[MinValueValidator(1)])
     description = models.TextField()
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from main.views import show_main, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id 
+from main.views import register, login_user, logout_user
+from main.views import plus, minus, remove
 
 app_name = 'main'
 
@@ -10,4 +12,10 @@ urlpatterns = [
     path('json/', show_json, name='show_json'), 
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('plus/<int:id>/', plus, name='plus'),
+    path('minus/<int:id>/', minus, name='minus'),
+    path('remove/<int:id>/', remove, name='remove'),
 ]
