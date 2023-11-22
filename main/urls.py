e0e7b5b0a875d42080f2ba0a7d9132e30b05fdc0
nano <marianosenduk@gmail.com>
2023-11-22 09:14:08 +0700

    redeploy

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from main.views import show_main, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id 
 from main.views import register, login_user, logout_user
 from main.views import plus, minus, remove
-from main.views import get_item_json, create_ajax, plus_ajax, minus_ajax, remove_ajax
+from main.views import get_item_json, create_ajax, plus_ajax, minus_ajax, remove_ajax, create_item_flutter
 
 app_name = 'main'
 
@@ -13,7 +13,7 @@ urlpatterns = [
     path('json/', show_json, name='show_json'), 
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
-    path('register/', register, name='register'),
+    path('register/', register, name='register'),   
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('plus/<int:id>/', plus, name='plus'),
@@ -24,4 +24,5 @@ urlpatterns = [
     path('plus-ajax/', plus_ajax, name='plus_ajax'),
     path('minus-ajax/', minus_ajax, name='minus_ajax'),
     path('remove-ajax/', remove_ajax, name='remove_ajax'),
+    path('create-flutter/', create_item_flutter, name='create_item_flutter'),
 ]
